2ecc4724aac0e971ca58959227eef8809f080f2f
HolodEg <egholodej@gmail.com>
2026-03-03 20:36:44 +0300

    Команды из второй практической работы

---
+++ user_data.sql
@@ -0,0 +1,11 @@
+insert into usr(first_name, middle_name, last_name, email) values
+('Иван', 'Иванович', 'Петров', 'ipetr@mail.ru'),
+('Алексей', 'Антович', 'Семёнов', 'alex_ant@gmail.com'),
+('Мария', 'Николаевна', 'Горохова', 'mary12345@yandex.ru'),
+('Даниил', 'Сегеевич', 'Волков', 'volkov.dan22@gmail.com'),
+('Иван', 'Петрович', 'Новиков', 'novikov_ip@bk.ru'),
+('Михаил', 'Денисович', 'Соловьёв', 'soloviev@yandex.ru'),
+('Анастасия', 'Игоревна', 'Лебедева', 'lebedeva.anastasiya@mail.ru'),
+('Екатерина', 'Павловна', 'Морозова', 'k.morozova@inbox.ru'),
+('Дмитрий', 'Алексеевич', 'Кузнецов', 'd.kuznetsov1978@mail.ru'),
+('Мария', 'Андреевна', 'Соколова', 'mascha.sokolova@yandex.ru');
